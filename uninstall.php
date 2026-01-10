c07bb58efcedeb43037f34c6a4a4f4b9d1ca8cb7
Atharva Badgujar <112540825+atharva-badgujar@users.noreply.github.com>
2026-01-10 12:08:43 +0530

    Clean up uninstall.php by removing comments and options
    Removed unnecessary comments and license options from the uninstall script.

--- uninstall.php
+++ uninstall.php
@@ -1,19 +1,14 @@
 <?php
 /**
  * Uninstall Script
- * Fired when the plugin is uninstalled
  *
  * @package SmartBot_BYOK
  */
 
-// If uninstall not called from WordPress, exit
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     exit;
 }
 
-/**
- * Delete plugin options
- */
 function smartbot_delete_options() {
     $options = array(
         'smartbot_provider',
@@ -22,8 +17,6 @@ function smartbot_delete_options() {
         'smartbot_brand_color',
         'smartbot_bot_name',
         'smartbot_bot_avatar',
-        'smartbot_license_key',
-        'smartbot_license_status',
     );
     
     foreach ( $options as $option ) {
@@ -31,13 +24,9 @@ function smartbot_delete_options() {
     }
 }
 
-/**
- * Delete transients
- */
 function smartbot_delete_transients() {
     global $wpdb;
     
-    // Delete all SmartBot transients
     $wpdb->query(
         "DELETE FROM {$wpdb->options} 
         WHERE option_name LIKE '_transient_smartbot_%' 
@@ -45,6 +34,5 @@ function smartbot_delete_transients() {
     );
 }
 
-// Execute cleanup
 smartbot_delete_options();
 smartbot_delete_transients();
